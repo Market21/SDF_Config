6e571674383d096c04a08737a67dd135ed7f1850
Market21 <marchettod@gmail.com>
2016-09-07 13:22:25 +0200

    1.0

--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace SDF_Config
 {
@@ -35,16 +36,33 @@ namespace SDF_Config
         {
             // Displays an OpenFileDialog so the user can select a Cursor.
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            openFileDialog1.Filter = "SDF Files|*.sdf";
+            openFileDialog1.Filter = "NIVSSDF Files|*.nivssdf";
             openFileDialog1.Title = "Select a SDF File";
+            string sFileSDFName = "";
 
             if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                System.IO.StreamReader sr = new
-                System.IO.StreamReader(openFileDialog1.FileName);
-                MessageBox.Show(sr.ReadToEnd());
-                sr.Close();
+                sFileSDFName = openFileDialog1.FileName;
+                label_name_sdf.Text = sFileSDFName;
             }
+            
+            //Lecture XML
+            XmlTextReader reader = new XmlTextReader(sFileSDFName);
+            while (reader.Read())
+            {
+                switch(reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        listBox1.Items.Add(reader.Name);
+                        break;
+
+                    case XmlNodeType.Text:
+                        listBox1.Items.Add(reader.Value);
+                        break;
+                }
+            }
+            
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
